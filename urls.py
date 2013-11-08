b96f66a99fc8f60454acf7297e4f9467565d684a
Cawb07 <ngrouse77@gmail.com>
2013-11-07 23:52:31 -0500

    Another incorrect import fixed

--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ Created on Nov 7, 2013
 @author: Cawb07
 '''
 from django.conf.urls import patterns, url
-from fieldtest import views
+from mtainfo import views
 
 urlpatterns = patterns('',
     url(r'^$', views.choice, name='choice'),
